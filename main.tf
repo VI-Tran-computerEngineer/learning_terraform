3846a86112119dabe9084ce7dc0f7d0252f54e73
ViTran OP5 <tranlongvi.ce@gmail.com>
2024-04-12 00:09:15 +0700

    Add new configs

--- main.tf
+++ main.tf
@@ -1,3 +1,16 @@
+# Define public module
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+  name = "default-us-east-vpc"
+  cidr = "172.31.0.0/16"
+
+  azs = ["us-east-2a", "us-east-2b", "us-east-2c", "us-east-2d", "us-east-2e", "us-east-2f"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+  # one_nat_gateway_per_az = true
+}
+
 data "aws_ami" "app_ami" {
   most_recent = true
 
@@ -16,7 +29,7 @@ data "aws_ami" "app_ami" {
 
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
-  instance_type = "t3.nano"
+  instance_type = var.environment_instance_type["DEV"]
 
   tags = {
     Name = "HelloWorld"
--- variables.tf
+++ variables.tf
@@ -1,4 +1,26 @@
-#variable "instance_type" {
-#  description = "Type of EC2 instance to provision"
-#  default     = "t3.nano"
-#}
+##############################################
+# Sensitive data variables
+variable "aws_access_key" {
+  sensitive = true
+}
+
+variable "aws_secret_key" {
+  sensitive = true
+}
+
+
+##############################################
+# Environment configuration variables
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "environment_instance_type" {
+  type = map(string)
+  default = {
+    "DEV" = "t2.micro",
+    "QA" = "t2.micro",
+    "STAGE" = "t2.micro",
+    "PROD" = "t2.micro"
+  }
+}
